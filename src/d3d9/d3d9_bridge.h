7c832935190435d3e1e6abf27d37d8a5296b73d3
Alpyne <alpyne.dreams@gmail.com>
2021-09-11 17:03:59 -0700

    [d3d8] Add UUID for D3D9Bridge

--- src/d3d9/d3d9_bridge.h
+++ src/d3d9/d3d9_bridge.h
@@ -11,7 +11,6 @@ namespace dxvk {
 
   class D3D9Bridge {
 
-
   public:
     D3D9Bridge(D3D9DeviceEx* pDevice) : m_device(pDevice) {}
 
@@ -25,3 +24,7 @@ namespace dxvk {
   };
 
 }
+
+#if !defined(_MSC_VER)
+__CRT_UUID_DECL(dxvk::D3D9Bridge, 0xD3D9ACAB, 0x42A9, 0x4C1E, 0xAA, 0x97, 0xDE, 0xAD, 0xFA, 0xDE, 0xD4, 0x20);
+#endif
