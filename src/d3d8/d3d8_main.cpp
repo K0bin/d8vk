2b6b9ed9091dcea95e277bdfcee4d3d2c845101e
Robin Kertels <robin.kertels@gmail.com>
2022-10-21 01:59:16 +0200

    fix build

--- src/d3d8/d3d8_main.cpp
+++ src/d3d8/d3d8_main.cpp
@@ -4,35 +4,14 @@
 #include "d3d8_interface.h"
 //#include "d3d9_shader_validator.h"
 
+#include "../d3d9/d3d9_interface.h"
+
 class D3DFE_PROCESSVERTICES;
 using PSGPERRORID = UINT;
 
-namespace dxvk {
-  Logger Logger::s_instance("d3d8.log");
-
-  HRESULT CreateD3D8(
-          UINT         SDKVersion,
-          IDirect3D8** ppDirect3D8) {
-    if (!ppDirect3D8)
-      return D3DERR_INVALIDCALL;
-
-    D3D9InterfaceEx* d3d9Iface = ref(new D3D9InterfaceEx( Extended ));
-    *ppDirect3D8 = d3d9Iface->GetD3D8Iface();
-
-    return D3D_OK;
-  }
-}
-
 extern "C" {
-
-  DLLEXPORT IDirect3D8* __stdcall Direct3DCreate8(UINT nSDKVersion) {
-    //dxvk::CreateD3D9(false, &pDirect3D);
-
-    dxvk::Logger::trace("Direct3DCreate8 called");
-
-    IDirect3D8* pDirect3D = nullptr;
-    dxvk::CreateD3D8(false, &pDirect3D);
-
-    return pDirect3D;
+  DLLEXPORT d3d8::IDirect3D8* __stdcall Direct3DCreate8(UINT nSDKVersion) {
+    IDirect3D9* d3d9Iface = Direct3DCreate9(nSDKVersion);
+    return static_cast<dxvk::D3D9InterfaceEx*>(d3d9Iface)->GetD3D8Iface();
   }
 }
